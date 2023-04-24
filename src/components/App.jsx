421103058a3609ade3d4a299c7c07a6b34a045c2
Julie Shapovalenko <jools.shapo@gmail.com>
2023-04-24 21:11:02 +0300

    update total count

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,8 +20,16 @@ export class App extends Component {
   };
 
   countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+    // const { good, neutral, bad } = this.state;
+    // return good + neutral + bad;
+
+    const values = Object.values(this.state);
+    return values.reduce((acc, value) => acc + value, 0);
+  };
+
+  countTotal = () => {
+    const values = Object.values(this.state);
+    return values.reduce((acc, value) => acc + value, 0);
   };
 
   countPositiveFeedbackPercentage = () => {
